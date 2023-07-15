efe6203f85229f0c14e45f33982cf7876ce7ba0b
hirwajeaneric <hirwaminerve25@gmail.com>
2023-07-15 18:46:03 +0200

    Modification of models

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const user = require('./user.routes');
 const mcc = require('./mcc.routes');
-const production = require('./production.routes');
+const milkProduction = require('./manureProduction.routes');
+const manureProduction = require('./milkProduction.routes');
 
 router.use('/user', user);
 router.use('/mcc', mcc);
-router.use('/production', production);
+router.use('/manure', manureProduction);
+router.use('/milk', manureProduction);
 
 module.exports = router;
